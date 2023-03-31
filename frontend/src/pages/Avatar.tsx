ce99fcfef2bc8a1ab4d16b89975e3c58b8b28f2f
sanyhe <sanyhew1097618435@163.com>
2023-03-31 23:37:28 +0800

    feat: Add user's avatar uploding in user page.

---
+++ frontend/src/pages/Avatar.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+interface AvatarProps {
+    setImage: (image: File) => void;
+}
+
+const Avatar = ({setImage}: AvatarProps) => {
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const image = e.target.files?.[0];
+        if (image) {
+            console.log('URL.createObjectURL(image): ' + URL.createObjectURL(image));
+            setImagePreview(URL.createObjectURL(image));
+            console.log('imagePreview: ' + imagePreview);
+            setImage(image);
+        }
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '120px', height: '120px', borderRadius: '50%', overflow: 'hidden', backgroundColor: 'grey', cursor: 'pointer'}}>
+            {imagePreview ? (
+                <img src={imagePreview} alt="user profile" />
+            ) : (
+                <span>Upload Image</span>
+            )}
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                // style={{ display: "none"}}
+            />
+        </div>
+    );
+};
+
+export default Avatar;

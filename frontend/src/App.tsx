cc5b6cb655641218a2fe71144d310202079d6edf
Hollie7 <1139872552@qq.com>
2023-04-18 17:24:13 +0800

    feat: add router for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserPage from "./pages/UserPage";
 
+const Test = () => {
+  return <div>Test</div>;
+}
 
 const App = () => {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Test />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/user" element={<UserPage />} />
+          <Route path="*" element={<div>404</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -46,7 +46,8 @@ module.exports = {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    hotOnly: true,
+    historyApiFallback: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devtool: "eval-cheap-source-map"

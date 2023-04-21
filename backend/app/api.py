5876baa548d888ce0ed39ee8541efad219d13a48
Fazzie <1240419984@qq.com>
2023-04-21 16:33:22 +0800

    add login router

--- backend/app/api.py
+++ backend/app/api.py
@@ -8,7 +8,7 @@ from PIL import Image
 import io
 from datetime import timedelta
 
-from .routers import users, image_generation, items
+from .routers import users, image_generation, items, login
 from .dependencies import get_query_token
 from .schemas.users import Token
 from .config import ACCESS_TOKEN_EXPIRE_MINUTES
@@ -24,6 +24,7 @@ app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(items.router)
+app.include_router(login.router)
 app.include_router(image_generation.router)
 
 origins = [

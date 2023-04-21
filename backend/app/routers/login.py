76d2e6e360b890c3b5feecb01d2b0dc53e4af816
Fazzie <1240419984@qq.com>
2023-04-21 16:33:22 +0800

    add login router

---
+++ backend/app/routers/login.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Depends, HTTPException, APIRouter
+from pydantic import BaseModel, EmailStr
+
+router = APIRouter(
+    prefix="/login",
+    tags=["login"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+def fake_authentication(email: str, password: str) -> bool:
+    # 实现真正的身份验证逻辑
+
+    if email is not None and password is not None:
+        return True
+
+    if email == "user@example.com" and password == "password":
+        return True
+    
+    return False
+
+@router.post("/login")
+async def login(login_request: LoginRequest):
+    is_authenticated = fake_authentication(login_request.email, login_request.password)
+    if not is_authenticated:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    return {"detail": "Successfully logged in"}

786b07ae83c63d0ad44845a0598af2c092b2460d
Fazzie <1240419984@qq.com>
2023-04-25 20:40:31 +0800

    fix imagen

--- backend/app/routers/imagen.py
+++ backend/app/routers/imagen.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 import requests
 from pydantic import BaseModel
 from typing import Optional
-import PIL
+import base64
+from PIL import Image
 import io
 
 from ..dependencies import get_db
@@ -17,25 +18,20 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-URL = {
+MODEL_URL = {
     '2D': "https://api-inference.huggingface.co/models/Fazzie/PokemonGAI",
     '3D': "https://api-inference.huggingface.co/models/Timmahw/SD2.1_Pokemon3D",
 }
 
+SPACE_URL = {
+    '2D': "https://fazzie-pokemongai.hf.space/run/predict",
+    '3D': "https://fazzie-timmahw-sd2-1-pokemon3d.hf.space/run/predict",
+}
+
 headers = {"Authorization": "Bearer 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"}
 
-class Payload(BaseModel):
-    inputs: str
-
-# def query(payload: Payload, type: str = '2D'):
-#     url = URL[type]
-#     response = requests.post(url, headers=headers, json=payload.dict())
-#     if response.status_code != 200:
-#         raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, payload))
-#     return response.content
-
-def query(prompt: str, type: str, name: str):
-    url = URL[type]
+def query_model(prompt: str, type: str, name: str):
+    url = MODEL_URL[type]
     response = requests.post(url, headers=headers, json={"inputs": f"{prompt}"})
     if response.status_code != 200:
         with open('.....assert/image.png', 'rb') as f:
@@ -44,6 +40,21 @@ def query(prompt: str, type: str, name: str):
         # raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, {'prompt': prompt, 'type': type, 'name': name}))
     return response.content
 
+def query_space(prompt: str, type: str, name: str):
+    url = SPACE_URL[type]
+
+    response = requests.post(url, headers=headers, json={
+    "data": [
+        f"{prompt}",
+    ]}).json()
+
+    if response.status_code != 200:
+        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, {'prompt': prompt, 'type': type, 'name': name}))
+    
+    data = response["data"][0]
+    image_data = data.split(",")[1]
+    image_bytes = base64.b64decode(image_data)
+    return image_bytes
 
 class Imagen(BaseModel):
     prompt: str = 'fire greninja, wings, (fire claws), smoke, cityscape'
@@ -59,14 +70,17 @@ async def generate_image(
     ) -> Response:
 
     try:
-        image_bytes = query(imagen.prompt, imagen.pokeType, imagen.pokeName)
-    except Exception as e:
-        return {
-            "status": 500,
-            "message": str(e)
-        }
+        image_bytes = query_space(imagen.prompt, imagen.pokeType, imagen.pokeName)
+    except:
+        try:
+            image_bytes = query_model(imagen.prompt, imagen.pokeType, imagen.pokeName)
+        except Exception as e:
+            return {
+                "status": 500,
+                "message": str(e)
+            }
     
-    image = PIL.Image.open(io.BytesIO(image_bytes))
+    image = Image.open(io.BytesIO(image_bytes))
     image.save(f"{imagen.pokeName}.png")
     
     temp_image = ImageCreate(**imagen.dict(), image_store=image_bytes)

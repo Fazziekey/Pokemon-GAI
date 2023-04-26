f04f860999e6c7b4cd81aa8572321602f7fbddd4
Fazzie-Maqianli <55798671+Fazziekey@users.noreply.github.com>
2023-04-26 17:27:29 +0800

    fix schema (#29)

--- backend/app/crud/images.py
+++ backend/app/crud/images.py
@@ -4,6 +4,7 @@ import requests
 from sqlalchemy.orm import Session
 
 from .. import models, schemas
+from ..schemas.images import ImageCreate
 
 UPLOAD_ACCOUT = os.environ['UPLOAD_ACCOUT']
 UPLOAD_TOKEN = os.environ['UPLOAD_TOKEN']
@@ -13,7 +14,7 @@ def get_images(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Image).offset(skip).limit(limit).all()
 
 
-def create_user_image(db: Session, image: schemas.images.ImageCreate, user_id: int):
+def create_user_image(db: Session, image: ImageCreate, user_id: int):
     db_image = models.Image(**image.dict(), owner_id=user_id)
     db.add(db_image)
     db.commit()
--- backend/app/crud/users.py
+++ backend/app/crud/users.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 
-from .. import models, schemas
+from .. import models
 from ..dependencies import get_password_hash
+from ..schemas.users import UserCreate
 
 
 def get_user_by_id(db: Session, user_id: str):
@@ -20,7 +21,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-def create_new_user(db: Session, user: schemas.users.UserCreate):
+def create_new_user(db: Session, user: UserCreate):
     hashed_password = get_password_hash(user.password)
     db_user = models.User(username=user.username,
                           full_name=user.full_name,
